e473c9a8d05028e67945bb69aed92353cc5f916b
DanielSon105 <daniel.g.barrido@gmail.com>
2015-10-07 14:22:02 -0500

    Initial Commit

---
+++ HallOfFrames/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HallOfFrames
+//
+//  Created by Daniel Barrido on 10/7/15.
+//  Copyright © 2015 AmarDanDemetriCo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HallOfFrames/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HallOfFrames
+//
+//  Created by Daniel Barrido on 10/7/15.
+//  Copyright © 2015 AmarDanDemetriCo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
